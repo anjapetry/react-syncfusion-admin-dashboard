83e55e750411995e42923cb4c173dd8ac15b23ea
AnjaP <treyjapan@pm.me>
2022-05-20 17:51:32 +0200

    adds data folder including dummy.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import 'App.css';
+import './App.css';
 
 const App = () => {
-	return <div>App</div>;
+	return <h1 className="underline text-3xl font-bold">App</h1>;
 };
 
 export default App;
