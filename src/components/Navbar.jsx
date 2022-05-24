3a5af1066d59695a9a6814191d2299436d08a4df
AnjaP <treyjapan@pm.me>
2022-05-24 19:19:01 +0200

    modify navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,30 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/dummy';
 
+import avatar from '../data/avatar.jpg';
+import { Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+	<TooltipComponent content={title} position="BottomCenter">
+		<button
+			type="button"
+			onClick={() => customFunc()}
+			style={{ color }}
+			className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+		>
+			<span
+				style={{ background: dotColor }}
+				className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+			/>
+			{icon}
+		</button>
+	</TooltipComponent>
+);
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative"></div>
 };
 
 export default Navbar;
